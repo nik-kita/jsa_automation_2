52538bcd57a55ed1c08074032cd8a01694f2d2a9
nik <nikita.moiseienko@onix-systems.com>
2020-12-07 16:09:36 +0200

    bad commit -> destroy branch

---
+++ src/main/java/main_package/ui/__USER__/page_objects/home/src/main/java/main_package/ui/__USER__/page_objects/home/Challenges.java
@@ -0,0 +1,101 @@
+
+package main_package.ui.__USER__.page_objects.home.src.main.java.main_package.ui.__USER__.page_objects.home;
+
+import main_package.data.Settings;
+import main_package.engine.Fly;
+import main_package.engine.test_engine.OnixUiAssert;
+import main_package.engine.ui_engine.OnixLocator;
+import main_package.engine.ui_engine.OnixPageObject;
+import main_package.engine.ui_engine.OnixWebDriver;
+import org.openqa.selenium.By;
+import main_package.data.S;
+
+public class Challenges extends OnixPageObject {
+    private String ENDPOINT_URL = ""; //TODO
+    public Challenges(OnixWebDriver driver) {
+        super(driver);
+        log.debug("[{}] page is open", "Challenges"); //TODO
+    }
+
+
+    @Override
+    public Challenges make(Fly fly) {
+        fly.make();
+        return this;
+    }
+
+    @Override
+    public Challenges openFromScratch() {
+        driver.get(Settings.BASE_URL);
+        //TODO
+        return this;
+    }
+    @Override
+    public Challenges openFromUrl() {
+        driver.get(Settings.BASE_URL + ENDPOINT_URL);
+        return this;
+    }
+    @Override
+    public Challenges check(OnixUiAssert onixUiAssert) {
+        for(OnixLocator l : OnixUiAssert.mergeArrays(
+                Challenges.Locator.values()
+                //TODO
+        )) {
+            onixUiAssert.softCheckCountOfElementByLocator(l, 1);
+        }
+        for(OnixLocator l : OnixUiAssert.mergeArrays(
+                Challenges.Locators.values()
+                //TODO
+        )) {
+            onixUiAssert.softCheckMinimumOfElementsByLocator(l, 1);
+        }
+        return this;
+    }
+
+
+    public enum Locator implements OnixLocator {
+        //TODO
+        ;
+        private By path;
+        private S[] actions;
+        Locator(By path) {
+            this.path = path;
+        }
+        Locator(By path, S... actions) {
+            this.path = path;
+            this.actions = actions;
+        }
+        @Override
+        public By getPath() {
+            return path;
+        }
+        @Override
+        public S[] specialActions() {
+           return actions;
+        }
+    }
+
+    public enum Locators implements OnixLocator {
+        //TODO
+        ;
+        private By path;
+        private S[] actions;
+        Locators(By path) {
+            this.path = path;
+        }
+        Locators(By path, S... actions) {
+            this.path = path;
+            this.actions = actions;
+        }
+        @Override
+        public By getPath() {
+            return path;
+        }
+        @Override
+        public S[] specialActions() {
+           return actions;
+        }
+    }
+
+}
+
---
+++ src/test/java/test_package/ui/unit/ui/__USER__/page_objects/home/src/main/java/main_package/ui/__USER__/page_objects/home/ChallengesTest.java
@@ -0,0 +1,50 @@
+
+package test_package.ui.unit.ui.__USER__.page_objects.home.src.main.java.main_package.ui.__USER__.page_objects.home;
+
+
+import main_package.engine.test_engine.OnixUiTestRunner;
+import main_package.engine.ui_engine.OnixLocator;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import main_package.ui.__USER__.page_objects.home.src.main.java.main_package.ui.__USER__.page_objects.home.Challenges;
+
+
+public class ChallengesTest extends OnixUiTestRunner {
+    Challenges challenges;
+    @BeforeClass
+    public void openChallenges() {
+        //TODO
+        //challenges = openSite();
+    }
+
+    @Test(dataProvider = "getUnique")
+    public void unique(OnixLocator locator) {
+        onixUiAssert.
+                checkCountOfElementByLocator(locator, 1);
+    }
+
+    @DataProvider
+    public Object[] getUnique() {
+        return mergeArrays(
+                Challenges.Locator.values()
+                //TODO
+        );
+    }
+
+    @Test(dataProvider = "getRepeated")
+    public void repeated(OnixLocator locator) {
+        onixUiAssert.
+                checkMinimumOfElementsByLocator(locator, 1);
+    }
+
+    @DataProvider
+    public Object[] getRepeated() {
+        return mergeArrays(
+                Challenges.Locators.values()
+                //TODO
+        );
+    }
+}
+
