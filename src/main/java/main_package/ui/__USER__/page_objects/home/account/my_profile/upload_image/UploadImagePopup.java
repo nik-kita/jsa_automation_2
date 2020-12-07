ba46f8aba19275f6f63d15d616599bfe5bee27f3
nik <nikita.moiseienko@onix-systems.com>
2020-12-07 16:28:48 +0200

    LocalFiles UploadImagePopup interface

---
+++ src/main/java/main_package/ui/__USER__/page_objects/home/account/my_profile/upload_image/UploadImagePopup.java
@@ -0,0 +1,64 @@
+
+package main_package.ui.__USER__.page_objects.home.account.my_profile.upload_image;
+
+import main_package.data.S;
+import main_package.engine.ui_engine.OnixLocator;
+import main_package.engine.ui_engine.OnixPart;
+import main_package.engine.ui_engine.OnixWebDriver;
+import main_package.ui.__USER__.page_objects.home.account.my_profile.MyProfile;
+import org.openqa.selenium.By;
+
+public interface UploadImagePopup extends OnixPart {
+
+    default MyProfile close() {
+        OnixWebDriver driver = getDriver();
+        driver.waitToClick(UploadImagePopupLtr.CLOSE).click();
+        return new MyProfile(driver);
+    }
+
+    enum UploadImagePopupLtr implements OnixLocator {
+        CLOSE(By.cssSelector(".uploadcare--dialog__close")),
+        ;
+        private By path;
+        private S[] actions;
+        UploadImagePopupLtr(By path) {
+            this.path = path;
+        }
+        UploadImagePopupLtr(By path, S... actions) {
+            this.path = path;
+            this.actions = actions;
+        }
+        @Override
+        public By getPath() {
+            return path;
+        }
+        @Override
+        public S[] specialActions() {
+            return actions;
+        }
+    }
+
+    enum UploadImagePopupLtrs implements OnixLocator {
+        //TODO
+        ;
+        private By path;
+        private S[] actions;
+        UploadImagePopupLtrs(By path) {
+            this.path = path;
+        }
+        UploadImagePopupLtrs(By path, S... actions) {
+            this.path = path;
+            this.actions = actions;
+        }
+        @Override
+        public By getPath() {
+            return path;
+        }
+        @Override
+        public S[] specialActions() {
+            return actions;
+        }
+    }
+}
+
+    
---
+++ src/test/java/test_package/ui/unit/ui/__USER__/page_objects/home/account/my_profile/upload_image/LocalFilesTest.java
@@ -0,0 +1,50 @@
+
+package test_package.ui.unit.ui.__USER__.page_objects.home.account.my_profile.upload_image;
+
+
+import main_package.engine.test_engine.OnixUiTestRunner;
+import main_package.engine.ui_engine.OnixLocator;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import main_package.ui.__USER__.page_objects.home.account.my_profile.upload_image.LocalFiles;
+
+
+public class LocalFilesTest extends OnixUiTestRunner {
+    LocalFiles localFiles;
+    @BeforeClass
+    public void openLocalFiles() {
+        //TODO
+        //localFiles = openSite();
+    }
+
+    @Test(dataProvider = "getUnique")
+    public void unique(OnixLocator locator) {
+        onixUiAssert.
+                checkCountOfElementByLocator(locator, 1);
+    }
+
+    @DataProvider
+    public Object[] getUnique() {
+        return mergeArrays(
+                LocalFiles.Locator.values()
+                //TODO
+        );
+    }
+
+    @Test(dataProvider = "getRepeated")
+    public void repeated(OnixLocator locator) {
+        onixUiAssert.
+                checkMinimumOfElementsByLocator(locator, 1);
+    }
+
+    @DataProvider
+    public Object[] getRepeated() {
+        return mergeArrays(
+                LocalFiles.Locators.values()
+                //TODO
+        );
+    }
+}
+
