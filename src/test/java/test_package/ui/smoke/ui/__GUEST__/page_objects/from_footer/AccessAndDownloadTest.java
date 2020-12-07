6a98364b766d4b478f2d84c067b62251472b3f83
nik <nikita.moiseienko@onix-systems.com>
2020-12-07 14:15:03 +0200

    AccessAndDownload

---
+++ src/test/java/test_package/ui/smoke/ui/__GUEST__/page_objects/from_footer/AccessAndDownloadTest.java
@@ -0,0 +1,19 @@
+
+package test_package.ui.smoke.ui.__GUEST__.page_objects.from_footer;
+
+import main_package.engine.test_engine.OnixUiTestRunner;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import main_package.ui.__GUEST__.page_objects.from_footer.AccessAndDownload;
+
+
+public class AccessAndDownloadTest extends OnixUiTestRunner {
+    AccessAndDownload accessAndDownload;
+    @BeforeMethod
+    public void openAccessAndDownload() {
+        //TODO
+    }
+    //TODO
+}
+
