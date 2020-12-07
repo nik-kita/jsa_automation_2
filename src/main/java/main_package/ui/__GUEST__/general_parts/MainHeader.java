f45fa65458e2030ac69e373e4028a5e3369db9b7
nik <volsknik@gmail.com>
2020-12-07 23:56:05 +0200

    MainHeader

--- src/main/java/main_package/ui/__GUEST__/general_parts/MainHeader.java
+++ src/main/java/main_package/ui/__GUEST__/general_parts/MainHeader.java
@@ -2,13 +2,93 @@
 package main_package.ui.__GUEST__.general_parts;
 
 import main_package.data.S;
+import main_package.engine.test_engine.OnixUiAssert;
 import main_package.engine.ui_engine.OnixLocator;
 import main_package.engine.ui_engine.OnixPart;
+import main_package.engine.ui_engine.OnixWebDriver;
+import main_package.ui.__GUEST__.page_objects.main.Challenge;
+import main_package.ui.__GUEST__.page_objects.main.Main;
+import main_package.ui.__GUEST__.page_objects.main.Pricing;
+import main_package.ui.__GUEST__.page_objects.main.Transformations;
+import main_package.ui.__GUEST__.page_objects.main.login.CreateAccount;
+import main_package.ui.__GUEST__.page_objects.main.login.Login;
+import main_package.ui.__GUEST__.page_objects.main.my_podcast.MyPodcast;
 import org.openqa.selenium.By;
+
+import static main_package.ui.__GUEST__.general_parts.MainHeader.MainHeaderLtr.*;
 
 public interface MainHeader extends OnixPart {
 
-    //TODO
+    default Main clickHeaderJsaLogo() {
+        OnixWebDriver driver = getDriver();
+        driver.findElement(JSA_LOGO).click();
+        getLog().info("click [Jsa logo] button in header");
+        return new Main(driver);
+    }
+    default Main clickHeaderJsaLogo(OnixUiAssert onixUiAssert) {
+        return clickHeaderJsaLogo().check(onixUiAssert);
+    }
+
+    default Challenge clickHeaderChallenge() {
+        OnixWebDriver driver = getDriver();
+        driver.findElement(CHALLENGE_HEADER_BUTTON).
+                click();
+        getLog().info("click [Challenge] button in header");
+        return new Challenge(driver);
+    }
+    default Challenge clickHeaderChallenge(OnixUiAssert onixUiAssert) {
+        return clickHeaderChallenge().check(onixUiAssert);
+    }
+    default Pricing clickHeaderPricing() {
+        OnixWebDriver driver = getDriver();
+        driver.findElement(PRICING_HEADER_BUTTON).
+                click();
+        getLog().info("click [Pricing] button in header");
+        return new Pricing(driver);
+    }
+    default Pricing clickHeaderPricing(OnixUiAssert onixUiAssert) {
+        return clickHeaderPricing().check(onixUiAssert);
+    }
+    default Transformations clickHeaderTransformations() {
+        OnixWebDriver driver = getDriver();
+        driver.findElement(TRANSFORMATIONS_HEADER_BUTTON).
+                click();
+        getLog().info("click [Transformations] button in header");
+        return new Transformations(driver);
+    }
+    default Transformations clickHeaderTransformations(OnixUiAssert onixUiAssert) {
+        return clickHeaderTransformations().check(onixUiAssert);
+    }
+    default Login clickHeaderLogin() {
+        OnixWebDriver driver = getDriver();
+        driver.findElement(LOGIN_HEADER_BUTTON).click();
+        getLog().info("click [Login] button in header");
+        return new Login(driver);
+    }
+    default Login clickHeaderLogin(OnixUiAssert onixUiAssert) {
+        return clickHeaderLogin().check(onixUiAssert);
+    }
+
+    default CreateAccount clickHeaderJoinNow() {
+        OnixWebDriver driver = getDriver();
+        driver.findElement(JOIN_NOW_HEADER_BUTTON).click();
+        getLog().info("click [Join now] button in header");
+        return new CreateAccount(driver);
+    }
+    default CreateAccount clickHeaderJoinNow(OnixUiAssert onixUiAssert) {
+        return clickHeaderJoinNow().check(onixUiAssert);
+    }
+
+    default MyPodcast clickHeaderMyPodcast() {
+        OnixWebDriver driver = getDriver();
+        driver.findElement(MY_PODCAST_HEADER_BUTTON).click();
+        getLog().info("click [My Podcast] button in header");
+        return new MyPodcast(driver);
+    }
+    default MyPodcast clickHeaderMyPodcast(OnixUiAssert onixUiAssert) {
+        return clickHeaderMyPodcast().check(onixUiAssert);
+    }
+
 
     enum MainHeaderLtr implements OnixLocator {
         JSA_LOGO(By.cssSelector("a[href='/'] img")),
