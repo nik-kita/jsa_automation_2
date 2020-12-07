efd705a882357c24e9a47a181b93c8a43bf3ceb1
nik <nikita.moiseienko@onix-systems.com>
2020-12-07 15:12:55 +0200

    FacebookGroupPage

---
+++ src/test/java/test_package/ui/smoke/ui/__related_sites__/FacebookGroupPageTest.java
@@ -0,0 +1,19 @@
+
+package test_package.ui.smoke.ui.__related_sites__;
+
+import main_package.engine.test_engine.OnixUiTestRunner;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import main_package.ui.__related_sites__.FacebookGroupPage;
+
+
+public class FacebookGroupPageTest extends OnixUiTestRunner {
+    FacebookGroupPage facebookGroupPage;
+    @BeforeMethod
+    public void openFacebookGroupPage() {
+        //TODO
+    }
+    //TODO
+}
+
