db0c16887fdcb30bdc4dc0e04f5e69a2e7e33739
nik <nikita.moiseienko@onix-systems.com>
2020-12-07 14:51:49 +0200

    SpotifyPage

---
+++ src/main/java/main_package/ui/__related_sites__/SpotifyPage.java
@@ -0,0 +1,101 @@
+
+package main_package.ui.__related_sites__;
+
+import main_package.data.Settings;
+import main_package.engine.Fly;
+import main_package.engine.test_engine.OnixUiAssert;
+import main_package.engine.ui_engine.OnixLocator;
+import main_package.engine.ui_engine.OnixPageObject;
+import main_package.engine.ui_engine.OnixWebDriver;
+import org.openqa.selenium.By;
+import main_package.data.S;
+
+public class SpotifyPage extends OnixPageObject {
+    private String ENDPOINT_URL = ""; //TODO
+    public SpotifyPage(OnixWebDriver driver) {
+        super(driver);
+        log.debug("[{}] page is open", "SpotifyPage"); //TODO
+    }
+
+
+    @Override
+    public SpotifyPage make(Fly fly) {
+        fly.make();
+        return this;
+    }
+
+    @Override
+    public SpotifyPage openFromScratch() {
+        driver.get(Settings.BASE_URL);
+        //TODO
+        return this;
+    }
+    @Override
+    public SpotifyPage openFromUrl() {
+        driver.get(Settings.BASE_URL + ENDPOINT_URL);
+        return this;
+    }
+    @Override
+    public SpotifyPage check(OnixUiAssert onixUiAssert) {
+        for(OnixLocator l : OnixUiAssert.mergeArrays(
+                SpotifyPage.Locator.values()
+                //TODO
+        )) {
+            onixUiAssert.softCheckCountOfElementByLocator(l, 1);
+        }
+        for(OnixLocator l : OnixUiAssert.mergeArrays(
+                SpotifyPage.Locators.values()
+                //TODO
+        )) {
+            onixUiAssert.softCheckMinimumOfElementsByLocator(l, 1);
+        }
+        return this;
+    }
+
+
+    public enum Locator implements OnixLocator {
+        //TODO
+        ;
+        private By path;
+        private S[] actions;
+        Locator(By path) {
+            this.path = path;
+        }
+        Locator(By path, S... actions) {
+            this.path = path;
+            this.actions = actions;
+        }
+        @Override
+        public By getPath() {
+            return path;
+        }
+        @Override
+        public S[] specialActions() {
+           return actions;
+        }
+    }
+
+    public enum Locators implements OnixLocator {
+        //TODO
+        ;
+        private By path;
+        private S[] actions;
+        Locators(By path) {
+            this.path = path;
+        }
+        Locators(By path, S... actions) {
+            this.path = path;
+            this.actions = actions;
+        }
+        @Override
+        public By getPath() {
+            return path;
+        }
+        @Override
+        public S[] specialActions() {
+           return actions;
+        }
+    }
+
+}
+
