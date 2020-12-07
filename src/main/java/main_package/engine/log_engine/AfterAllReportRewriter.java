ed87a5b3791a359c7a13606358f01efa8e3eb609
nik <volsknik@gmail.com>
2020-12-07 08:55:59 +0200

    Runtime.getRuntime().addShutdownHook(new MyClass(//TODO))

---
+++ src/main/java/main_package/engine/log_engine/AfterAllReportRewriter.java
@@ -0,0 +1,7 @@
+package main_package.engine.log_engine;
+
+public class AfterAllReportRewriter extends Thread {
+    public void run() {
+        System.out.println("\n=====================\n\n//TODO\n\n=====================");
+    }
+}
--- src/main/java/main_package/engine/test_engine/OnixTestRunner.java
+++ src/main/java/main_package/engine/test_engine/OnixTestRunner.java
@@ -1,10 +1,14 @@
 package main_package.engine.test_engine;
 
 import main_package.engine.BaseClass;
+import main_package.engine.log_engine.AfterAllReportRewriter;
 import org.testng.annotations.Listeners;
 
 @Listeners(org.uncommons.reportng.HTMLReporter.class)
 public abstract class OnixTestRunner extends BaseClass {
+    {
+        Runtime.getRuntime().addShutdownHook(new AfterAllReportRewriter());
+    }
     public Object[] mergeArrays(Object[]... arrays) {
         int length = 0;
         for (Object[] array : arrays) {
