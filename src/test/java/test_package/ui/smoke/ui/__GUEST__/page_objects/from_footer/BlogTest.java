694a3ed88980127bd9973b7775c944adaaf81e4c
nik <nikita.moiseienko@onix-systems.com>
2020-12-07 13:51:52 +0200

    Blog

---
+++ src/test/java/test_package/ui/smoke/ui/__GUEST__/page_objects/from_footer/BlogTest.java
@@ -0,0 +1,19 @@
+
+package test_package.ui.smoke.ui.__GUEST__.page_objects.from_footer;
+
+import main_package.engine.test_engine.OnixUiTestRunner;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import main_package.ui.__GUEST__.page_objects.from_footer.Blog;
+
+
+public class BlogTest extends OnixUiTestRunner {
+    Blog blog;
+    @BeforeMethod
+    public void openBlog() {
+        //TODO
+    }
+    //TODO
+}
+
