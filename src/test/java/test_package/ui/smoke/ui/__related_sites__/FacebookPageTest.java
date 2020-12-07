72471fd9a2475cd0377f34db0ab3595d772b3f78
nik <nikita.moiseienko@onix-systems.com>
2020-12-07 14:43:45 +0200

    FacebookPage

---
+++ src/test/java/test_package/ui/smoke/ui/__related_sites__/FacebookPageTest.java
@@ -0,0 +1,19 @@
+
+package test_package.ui.smoke.ui.__related_sites__;
+
+import main_package.engine.test_engine.OnixUiTestRunner;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import main_package.ui.__related_sites__.FacebookPage;
+
+
+public class FacebookPageTest extends OnixUiTestRunner {
+    FacebookPage facebookPage;
+    @BeforeMethod
+    public void openFacebookPage() {
+        //TODO
+    }
+    //TODO
+}
+
---
+++ src/test/java/test_package/ui/unit/ui/__related_sites__/FacebookPageTest.java
@@ -0,0 +1,50 @@
+
+package test_package.ui.unit.ui.__related_sites__;
+
+
+import main_package.engine.test_engine.OnixUiTestRunner;
+import main_package.engine.ui_engine.OnixLocator;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import main_package.ui.__related_sites__.FacebookPage;
+
+
+public class FacebookPageTest extends OnixUiTestRunner {
+    FacebookPage facebookPage;
+    @BeforeClass
+    public void openFacebookPage() {
+        //TODO
+        //facebookPage = openSite();
+    }
+
+    @Test(dataProvider = "getUnique")
+    public void unique(OnixLocator locator) {
+        onixUiAssert.
+                checkCountOfElementByLocator(locator, 1);
+    }
+
+    @DataProvider
+    public Object[] getUnique() {
+        return mergeArrays(
+                FacebookPage.Locator.values()
+                //TODO
+        );
+    }
+
+    @Test(dataProvider = "getRepeated")
+    public void repeated(OnixLocator locator) {
+        onixUiAssert.
+                checkMinimumOfElementsByLocator(locator, 1);
+    }
+
+    @DataProvider
+    public Object[] getRepeated() {
+        return mergeArrays(
+                FacebookPage.Locators.values()
+                //TODO
+        );
+    }
+}
+
