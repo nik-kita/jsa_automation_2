5e5cc42098d9978aec6ae774aa34fb87b07a3dbb
nik <nikita.moiseienko@onix-systems.com>
2020-12-07 16:34:31 +0200

    commit

---
+++ src/test/java/test_package/ui/smoke/ui/__USER__/page_objects/home/my_goal/MyGoalTest.java
@@ -0,0 +1,19 @@
+
+package test_package.ui.smoke.ui.__USER__.page_objects.home.my_goal;
+
+import main_package.engine.test_engine.OnixUiTestRunner;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import main_package.ui.__USER__.page_objects.home.my_goal.MyGoal;
+
+
+public class MyGoalTest extends OnixUiTestRunner {
+    MyGoal myGoal;
+    @BeforeMethod
+    public void openMyGoal() {
+        //TODO
+    }
+    //TODO
+}
+
