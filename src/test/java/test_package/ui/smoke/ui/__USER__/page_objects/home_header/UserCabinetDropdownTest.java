1d6caced4b2c46cbfe92b17b69e4394bbb0eedb3
nik <nikita.moiseienko@onix-systems.com>
2020-12-07 16:02:13 +0200

    UserCabinetDropdown

---
+++ src/test/java/test_package/ui/smoke/ui/__USER__/page_objects/home_header/UserCabinetDropdownTest.java
@@ -0,0 +1,19 @@
+
+package test_package.ui.smoke.ui.__USER__.page_objects.home_header;
+
+import main_package.engine.test_engine.OnixUiTestRunner;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import main_package.ui.__USER__.page_objects.home_header.UserCabinetDropdown;
+
+
+public class UserCabinetDropdownTest extends OnixUiTestRunner {
+    UserCabinetDropdown userCabinetDropdown;
+    @BeforeMethod
+    public void openUserCabinetDropdown() {
+        //TODO
+    }
+    //TODO
+}
+
