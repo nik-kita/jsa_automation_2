efb57254702155d477491a5ed6d4f8aa8e6ae56a
nik <nikita.moiseienko@onix-systems.com>
2020-12-07 14:27:31 +0200

    GooglePlayPage

---
+++ src/test/java/test_package/ui/unit/ui/__related_sites__/GooglePlayPageTest.java
@@ -0,0 +1,50 @@
+
+package test_package.ui.unit.ui.__related_sites__;
+
+
+import main_package.engine.test_engine.OnixUiTestRunner;
+import main_package.engine.ui_engine.OnixLocator;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import main_package.ui.__related_sites__.GooglePlayPage;
+
+
+public class GooglePlayPageTest extends OnixUiTestRunner {
+    GooglePlayPage googlePlayPage;
+    @BeforeClass
+    public void openGooglePlayPage() {
+        //TODO
+        //googlePlayPage = openSite();
+    }
+
+    @Test(dataProvider = "getUnique")
+    public void unique(OnixLocator locator) {
+        onixUiAssert.
+                checkCountOfElementByLocator(locator, 1);
+    }
+
+    @DataProvider
+    public Object[] getUnique() {
+        return mergeArrays(
+                GooglePlayPage.Locator.values()
+                //TODO
+        );
+    }
+
+    @Test(dataProvider = "getRepeated")
+    public void repeated(OnixLocator locator) {
+        onixUiAssert.
+                checkMinimumOfElementsByLocator(locator, 1);
+    }
+
+    @DataProvider
+    public Object[] getRepeated() {
+        return mergeArrays(
+                GooglePlayPage.Locators.values()
+                //TODO
+        );
+    }
+}
+
