86124b471781a25b770b9100e74cb41d63015259
nik <volsknik@gmail.com>
2020-12-07 22:47:01 +0200

    Challenge

--- src/main/java/main_package/ui/__USER__/page_objects/main/Challenge.java
+++ src/main/java/main_package/ui/__USER__/page_objects/main/Challenge.java
@@ -7,10 +7,12 @@ import main_package.engine.test_engine.OnixUiAssert;
 import main_package.engine.ui_engine.OnixLocator;
 import main_package.engine.ui_engine.OnixPageObject;
 import main_package.engine.ui_engine.OnixWebDriver;
+import main_package.ui.__USER__.general_parts.Footer;
+import main_package.ui.__USER__.general_parts.MainHeader;
 import org.openqa.selenium.By;
 import main_package.data.S;
 
-public class Challenge extends OnixPageObject {
+public class Challenge extends OnixPageObject implements Footer, MainHeader {
     private String ENDPOINT_URL = ""; //TODO
     public Challenge(OnixWebDriver driver) {
         super(driver);
@@ -38,14 +40,16 @@ public class Challenge extends OnixPageObject {
     @Override
     public Challenge check(OnixUiAssert onixUiAssert) {
         for(OnixLocator l : OnixUiAssert.mergeArrays(
-                Challenge.Locator.values()
-                //TODO
+                Challenge.Locator.values(),
+                Footer.FooterLtr.values(),
+                MainHeader.MainHeaderLtr.values()
         )) {
             onixUiAssert.softCheckCountOfElementByLocator(l, 1);
         }
         for(OnixLocator l : OnixUiAssert.mergeArrays(
-                Challenge.Locators.values()
-                //TODO
+                Challenge.Locators.values(),
+                Footer.FooterLtrs.values(),
+                MainHeader.MainHeaderLtrs.values()
         )) {
             onixUiAssert.softCheckMinimumOfElementsByLocator(l, 1);
         }
@@ -54,7 +58,11 @@ public class Challenge extends OnixPageObject {
 
 
     public enum Locator implements OnixLocator {
-        //TODO
+        CREATE_ACCOUNT_BUTTON_INNER_PAGE(By.cssSelector(".inner_page a[href='/users/register/']")),
+        EMAIL_INPUT_INNER_PAGE(By.cssSelector("input[name='email']")),
+        SIGN_UP_BUTTON_INNER_PAGE(By.cssSelector("button[onclick='submitRegisterInterest()']")),
+        PREMIUM_PLAN_LINK(By.cssSelector("[href='/users/pricing/']")),
+
         ;
         private By path;
         private S[] actions;
