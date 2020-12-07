b9465b82bb2d372ce90ba96334f7eec34b7ab1e7
nik <nikita.moiseienko@onix-systems.com>
2020-12-07 16:40:15 +0200

    workouts

---
+++ src/test/java/test_package/ui/smoke/ui/__USER__/page_objects/home_header/workouts/PersonalisedTest.java
@@ -0,0 +1,19 @@
+
+package test_package.ui.smoke.ui.__USER__.page_objects.home_header.workouts;
+
+import main_package.engine.test_engine.OnixUiTestRunner;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import main_package.ui.__USER__.page_objects.home_header.workouts.Personalised;
+
+
+public class PersonalisedTest extends OnixUiTestRunner {
+    Personalised personalised;
+    @BeforeMethod
+    public void openPersonalised() {
+        //TODO
+    }
+    //TODO
+}
+
---
+++ src/test/java/test_package/ui/unit/ui/__USER__/page_objects/home_header/workouts/BrowseTest.java
@@ -0,0 +1,50 @@
+
+package test_package.ui.unit.ui.__USER__.page_objects.home_header.workouts;
+
+
+import main_package.engine.test_engine.OnixUiTestRunner;
+import main_package.engine.ui_engine.OnixLocator;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import main_package.ui.__USER__.page_objects.home_header.workouts.Browse;
+
+
+public class BrowseTest extends OnixUiTestRunner {
+    Browse browse;
+    @BeforeClass
+    public void openBrowse() {
+        //TODO
+        //browse = openSite();
+    }
+
+    @Test(dataProvider = "getUnique")
+    public void unique(OnixLocator locator) {
+        onixUiAssert.
+                checkCountOfElementByLocator(locator, 1);
+    }
+
+    @DataProvider
+    public Object[] getUnique() {
+        return mergeArrays(
+                Browse.Locator.values()
+                //TODO
+        );
+    }
+
+    @Test(dataProvider = "getRepeated")
+    public void repeated(OnixLocator locator) {
+        onixUiAssert.
+                checkMinimumOfElementsByLocator(locator, 1);
+    }
+
+    @DataProvider
+    public Object[] getRepeated() {
+        return mergeArrays(
+                Browse.Locators.values()
+                //TODO
+        );
+    }
+}
+
