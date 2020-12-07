87166a7d635e526b0caaf5bbe8a36c76bc641988
nik <nikita.moiseienko@onix-systems.com>
2020-12-07 17:11:52 +0200

    AccountHeader

--- src/main/java/main_package/ui/__USER__/general_parts/home/account/AccountHeader.java
+++ src/main/java/main_package/ui/__USER__/general_parts/home/account/AccountHeader.java
@@ -4,14 +4,56 @@ package main_package.ui.__USER__.general_parts.home.account;
 import main_package.data.S;
 import main_package.engine.ui_engine.OnixLocator;
 import main_package.engine.ui_engine.OnixPart;
+import main_package.engine.ui_engine.OnixWebDriver;
+import main_package.ui.__GUEST__.page_objects.AfterLogout;
+import main_package.ui.__USER__.page_objects.home_header.home.Home;
+import main_package.ui.__USER__.page_objects.home_header.home.account.my_plan.MyPlan;
+import main_package.ui.__USER__.page_objects.main.Main;
+import main_package.ui.__USER__.page_objects.main.pricing.pricingplans.PricingPlans;
 import org.openqa.selenium.By;
+
+import static main_package.ui.__USER__.general_parts.home.account.AccountHeader.AccountHeaderLtr.*;
 
 public interface AccountHeader extends OnixPart {
 
-    //TODO
+    default Home clickHomeHeaderButton() {
+        OnixWebDriver onixWebDriver = getDriver();
+        onixWebDriver.findElement(HOME_HEADER_BUTTON).click();
+        getLog().debug("'Home' from header");
+        return new Home(onixWebDriver);
+    }
+    default Main clickJsaHeaderLogo() {
+        OnixWebDriver driver = getDriver();
+        driver.findElement(JSA_LOGO).click();
+        getLog().debug("'Main' from header");
+        return new Main(driver);
+    }
+    default MyPlan clickAccountHeaderButton() {
+        OnixWebDriver driver = getDriver();
+        driver.findElement(ACCOUNT_HEADER_BUTTON).click();
+        getLog().debug("'Account' from header");
+        return new MyPlan(driver);
+    }
+    default PricingPlans clickPricingPlansHeaderButton() {
+        OnixWebDriver driver = getDriver();
+        driver.findElement(PRICING_PLANS_HEADER_BUTTON).click();
+        getLog().debug("'PricingPlans' from header");
+        return new PricingPlans(driver);
+    }
+    default AfterLogout clickLogoutHeaderButton() {
+        OnixWebDriver driver = getDriver();
+        driver.findElement(LOGOUT_HEADER_BUTTON).click();
+        getLog().debug("'AfterLogout' from header");
+        return new AfterLogout(driver);
+    }
+
 
     enum AccountHeaderLtr implements OnixLocator {
-        //TODO
+        JSA_LOGO(By.cssSelector(".main-header [href='/']")),
+        HOME_HEADER_BUTTON(By.cssSelector(".nav [href='/users/']")),
+        ACCOUNT_HEADER_BUTTON(By.cssSelector(".nav [href='/users/account/']")),
+        PRICING_PLANS_HEADER_BUTTON(By.cssSelector(".nav [href='/users/pricing/']")),
+        LOGOUT_HEADER_BUTTON(By.xpath("//div[@class='nav']//a[contains(text(), 'Logout')]"))
         ;
         private By path;
         private S[] actions;
