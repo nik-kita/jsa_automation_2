f09686c2354267528f29bd331bb5b9d90b8c153d
nik <nikita.moiseienko@onix-systems.com>
2020-12-07 19:21:13 +0200

    EditTodayStepsPopup

--- src/main/java/main_package/ui/__USER__/page_objects/home_header/home/steps/EditTodayStepsPopup.java
+++ src/main/java/main_package/ui/__USER__/page_objects/home_header/home/steps/EditTodayStepsPopup.java
@@ -17,6 +17,33 @@ public class EditTodayStepsPopup extends OnixPageObject {
         log.debug("[{}] page is open", "EditTodayStepsPopup"); //TODO
     }
 
+    public Steps close() {
+        driver.findElement(Locator.CLOSE_BUTTON).click();
+        log.info("click [{}] close button in [{}] popup", "x", "Add Today's Steps");
+        if(driver.isElementPresent(Locator.CLOSE_BUTTON)) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                log.warn("Waiting for invisibility of modal window (popup).");
+            }
+        }
+        return new Steps(driver);
+    }
+
+    public Steps update(int steps) {
+        driver.findElement(Locator.TODAY_STEPS_INPUT).getSeleniumWebElement().clear();
+        driver.findElement(Locator.TODAY_STEPS_INPUT).sendKeys(String.valueOf(steps));
+        log.info("Write [{}] into [{}] input.", steps, "Today's Steps");
+        driver.findElement(Locator.UPDATE_BUTTON).click();
+        if(driver.isElementPresent(Locator.UPDATE_BUTTON)) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                log.warn("Waiting for invisibility of modal window (popup).");
+            }
+        }
+        return new Steps(driver);
+    }
 
     @Override
     public EditTodayStepsPopup make(Fly fly) {
@@ -54,7 +81,9 @@ public class EditTodayStepsPopup extends OnixPageObject {
 
 
     public enum Locator implements OnixLocator {
-        //TODO
+        CLOSE_BUTTON(By.cssSelector(".modal-window .btn img")),
+        TODAY_STEPS_INPUT(By.cssSelector("input[name='steps']")),
+        UPDATE_BUTTON(By.cssSelector(".todays_step_form .btn-primary"))
         ;
         private By path;
         private S[] actions;
