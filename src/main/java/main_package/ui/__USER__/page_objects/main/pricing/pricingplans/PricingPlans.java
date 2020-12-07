33e4b6b6d6e7c5b316735c59d06f57822278dca5
nik <volsknik@gmail.com>
2020-12-07 22:41:19 +0200

    PricingPlans

--- src/main/java/main_package/ui/__USER__/page_objects/main/pricing/pricingplans/PricingPlans.java
+++ src/main/java/main_package/ui/__USER__/page_objects/main/pricing/pricingplans/PricingPlans.java
@@ -7,14 +7,60 @@ import main_package.engine.test_engine.OnixUiAssert;
 import main_package.engine.ui_engine.OnixLocator;
 import main_package.engine.ui_engine.OnixPageObject;
 import main_package.engine.ui_engine.OnixWebDriver;
+import main_package.ui.__USER__.general_parts.Footer;
+import main_package.ui.__USER__.general_parts.home.account.AccountHeader;
+import main_package.ui.__USER__.page_objects.home_header.home.Home;
+import main_package.ui.__USER__.page_objects.main.pricing.pricingplans.payment_popup.PaymentMethodPopup;
 import org.openqa.selenium.By;
 import main_package.data.S;
 
-public class PricingPlans extends OnixPageObject {
+import static main_package.ui.__USER__.general_parts.home.account.AccountHeader.AccountHeaderLtr.HOME_HEADER_BUTTON;
+
+public class PricingPlans extends OnixPageObject implements Footer, AccountHeader {
     private String ENDPOINT_URL = ""; //TODO
     public PricingPlans(OnixWebDriver driver) {
         super(driver);
         log.debug("[{}] page is open", "PricingPlans"); //TODO
+    }
+
+    public Home goHome() {
+        driver.findElement(HOME_HEADER_BUTTON).click();
+        log.info("click [{}] button in home-header", "Home");
+        return new Home(driver);
+    }
+
+    public PaymentMethodPopup subscribeStandard() {
+        driver.scrollPageDown();
+        driver.findElement(Locator.SUBSCRIBE_BTN_STANDARD_PLAN).click();
+        log.info("click [{}] button in [{}] tab", "Subscribe", "Standard Plan");
+        return new PaymentMethodPopup(driver);
+    }
+    public PaymentMethodPopup subscribePremium() {
+        driver.scrollPageDown();
+        driver.findElement(Locator.SUBSCRIBE_BTN_PREMIUM_PLAN).click();
+        log.info("click [{}] button in [{}] tab", "Subscribe", "Premium Plan");
+        return new PaymentMethodPopup(driver);
+    }
+
+    public PaymentMethodPopup buyNow() {
+        clickBlocksButton();
+        driver.scrollPageDown();
+        driver.findElement(BlocksTabLocator.BUY_NOW_BUTTON).click();
+        log.info("click [{}] button in [{}] tab", "Buy Now", "Blocks");
+        return new PaymentMethodPopup(driver);
+    }
+    public PricingPlans clickBlocksButton() {
+        driver.scrollPageUp();
+        driver.findElement(Locator.BLOCKS_BUTTON).click();
+        log.info("click [{}] tab-button", "Blocks");
+        return this;
+    }
+
+    public PricingPlans clickSubscriptionsButton() {
+        driver.scrollPageUp();
+        driver.findElement(Locator.SUBSCRIPTIONS_BUTTON).click();
+        log.info("click [{}] button", "Subscription"); //TODO wtf? check! change log sms
+        return this;
     }
 
 
@@ -38,14 +84,16 @@ public class PricingPlans extends OnixPageObject {
     @Override
     public PricingPlans check(OnixUiAssert onixUiAssert) {
         for(OnixLocator l : OnixUiAssert.mergeArrays(
-                PricingPlans.Locator.values()
-                //TODO
+                PricingPlans.Locator.values(),
+                AccountHeader.AccountHeaderLtr.values(),
+                Footer.FooterLtr.values()
         )) {
             onixUiAssert.softCheckCountOfElementByLocator(l, 1);
         }
         for(OnixLocator l : OnixUiAssert.mergeArrays(
-                PricingPlans.Locators.values()
-                //TODO
+                PricingPlans.Locators.values(),
+                AccountHeader.AccountHeaderLtrs.values(),
+                Footer.FooterLtrs.values()
         )) {
             onixUiAssert.softCheckMinimumOfElementsByLocator(l, 1);
         }
@@ -54,7 +102,11 @@ public class PricingPlans extends OnixPageObject {
 
 
     public enum Locator implements OnixLocator {
-        //TODO
+        SUBSCRIPTIONS_BUTTON(By.xpath("//*[contains(text(), 'Subscriptions')]")),
+        BLOCKS_BUTTON(By.xpath("//*[contains(text(), 'Blocks')]")),
+        SUBSCRIBE_BTN_STANDARD_PLAN(By.id("standardSubscriptionButton")),
+        SUBSCRIBE_BTN_PREMIUM_PLAN(By.id("premiumSubscriptionButton")),
+
         ;
         private By path;
         private S[] actions;
@@ -94,6 +146,35 @@ public class PricingPlans extends OnixPageObject {
         @Override
         public S[] specialActions() {
            return actions;
+        }
+    }
+
+    //TODO
+
+    /**
+     * TODO
+     * make separate page object for 'Block Tab' !!!
+     */
+    public enum BlocksTabLocator implements OnixLocator {
+        BUY_NOW_BUTTON(By.cssSelector("#premiumBlockButton")),
+
+        ;
+        private By path;
+        private S[] actions;
+        BlocksTabLocator(By path) {
+            this.path = path;
+        }
+        BlocksTabLocator(By path, S... actions) {
+            this.path = path;
+            this.actions = actions;
+        }
+        @Override
+        public By getPath() {
+            return path;
+        }
+        @Override
+        public S[] specialActions() {
+            return actions;
         }
     }
 
