a7d908e5a5f1f407076a74ab26d36777ccd6f7b4
nik <nikita.moiseienko@onix-systems.com>
2020-12-07 16:38:41 +0200

    learn

---
+++ src/test/java/test_package/ui/smoke/ui/__USER__/page_objects/home_header/learn/SingleLearnVideoTest.java
@@ -0,0 +1,19 @@
+
+package test_package.ui.smoke.ui.__USER__.page_objects.home_header.learn;
+
+import main_package.engine.test_engine.OnixUiTestRunner;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import main_package.ui.__USER__.page_objects.home_header.learn.SingleLearnVideo;
+
+
+public class SingleLearnVideoTest extends OnixUiTestRunner {
+    SingleLearnVideo singleLearnVideo;
+    @BeforeMethod
+    public void openSingleLearnVideo() {
+        //TODO
+    }
+    //TODO
+}
+
---
+++ src/test/java/test_package/ui/smoke/ui/__USER__/page_objects/home_header/learn/ToCompleteTest.java
@@ -0,0 +1,19 @@
+
+package test_package.ui.smoke.ui.__USER__.page_objects.home_header.learn;
+
+import main_package.engine.test_engine.OnixUiTestRunner;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import main_package.ui.__USER__.page_objects.home_header.learn.ToComplete;
+
+
+public class ToCompleteTest extends OnixUiTestRunner {
+    ToComplete toComplete;
+    @BeforeMethod
+    public void openToComplete() {
+        //TODO
+    }
+    //TODO
+}
+
