8b9a6c1625d359cbd3ece96a97f0b8dc73f6c276
nik <volsknik@gmail.com>
2020-12-07 22:49:33 +0200

    FamousDailyEmails

--- src/main/java/main_package/ui/__USER__/page_objects/main/FamousDailyEmails.java
+++ src/main/java/main_package/ui/__USER__/page_objects/main/FamousDailyEmails.java
@@ -7,10 +7,12 @@ import main_package.engine.test_engine.OnixUiAssert;
 import main_package.engine.ui_engine.OnixLocator;
 import main_package.engine.ui_engine.OnixPageObject;
 import main_package.engine.ui_engine.OnixWebDriver;
+import main_package.ui.__USER__.general_parts.Footer;
+import main_package.ui.__USER__.general_parts.MainHeader;
 import org.openqa.selenium.By;
 import main_package.data.S;
 
-public class FamousDailyEmails extends OnixPageObject {
+public class FamousDailyEmails extends OnixPageObject implements MainHeader, Footer {
     private String ENDPOINT_URL = ""; //TODO
     public FamousDailyEmails(OnixWebDriver driver) {
         super(driver);
@@ -38,14 +40,16 @@ public class FamousDailyEmails extends OnixPageObject {
     @Override
     public FamousDailyEmails check(OnixUiAssert onixUiAssert) {
         for(OnixLocator l : OnixUiAssert.mergeArrays(
-                FamousDailyEmails.Locator.values()
-                //TODO
+                FamousDailyEmails.Locator.values(),
+                Footer.FooterLtr.values(),
+                MainHeader.MainHeaderLtr.values()
         )) {
             onixUiAssert.softCheckCountOfElementByLocator(l, 1);
         }
         for(OnixLocator l : OnixUiAssert.mergeArrays(
-                FamousDailyEmails.Locators.values()
-                //TODO
+                FamousDailyEmails.Locators.values(),
+                Footer.FooterLtrs.values(),
+                MainHeader.MainHeaderLtrs.values()
         )) {
             onixUiAssert.softCheckMinimumOfElementsByLocator(l, 1);
         }
@@ -54,7 +58,9 @@ public class FamousDailyEmails extends OnixPageObject {
 
 
     public enum Locator implements OnixLocator {
-        //TODO
+        EMAIL_INPUT(By.cssSelector("form input[name='email']")),
+        SIGN_UP_BUTTON(By.cssSelector("form button#submit")),
+        SUBSCRIBE_BUTTON(By.xpath("//button[contains(text(), 'Subscribe')]"))
         ;
         private By path;
         private S[] actions;
