4d289283a7832cfb598dc70a0171f30735d963b0
nik <nikita.moiseienko@onix-systems.com>
2020-12-07 16:21:01 +0200

    EmailPreferences myProfile ResetPasserdFrom...

---
+++ src/test/java/test_package/ui/smoke/ui/__USER__/page_objects/home/account/my_profile/ResetPasswordFromMyProfileTest.java
@@ -0,0 +1,19 @@
+
+package test_package.ui.smoke.ui.__USER__.page_objects.home.account.my_profile;
+
+import main_package.engine.test_engine.OnixUiTestRunner;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import main_package.ui.__USER__.page_objects.home.account.my_profile.ResetPasswordFromMyProfile;
+
+
+public class ResetPasswordFromMyProfileTest extends OnixUiTestRunner {
+    ResetPasswordFromMyProfile resetPasswordFromMyProfile;
+    @BeforeMethod
+    public void openResetPasswordFromMyProfile() {
+        //TODO
+    }
+    //TODO
+}
+
---
+++ src/test/java/test_package/ui/unit/ui/__USER__/page_objects/home/account/my_profile/MyProfileTest.java
@@ -0,0 +1,50 @@
+
+package test_package.ui.unit.ui.__USER__.page_objects.home.account.my_profile;
+
+
+import main_package.engine.test_engine.OnixUiTestRunner;
+import main_package.engine.ui_engine.OnixLocator;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import main_package.ui.__USER__.page_objects.home.account.my_profile.MyProfile;
+
+
+public class MyProfileTest extends OnixUiTestRunner {
+    MyProfile myProfile;
+    @BeforeClass
+    public void openMyProfile() {
+        //TODO
+        //myProfile = openSite();
+    }
+
+    @Test(dataProvider = "getUnique")
+    public void unique(OnixLocator locator) {
+        onixUiAssert.
+                checkCountOfElementByLocator(locator, 1);
+    }
+
+    @DataProvider
+    public Object[] getUnique() {
+        return mergeArrays(
+                MyProfile.Locator.values()
+                //TODO
+        );
+    }
+
+    @Test(dataProvider = "getRepeated")
+    public void repeated(OnixLocator locator) {
+        onixUiAssert.
+                checkMinimumOfElementsByLocator(locator, 1);
+    }
+
+    @DataProvider
+    public Object[] getRepeated() {
+        return mergeArrays(
+                MyProfile.Locators.values()
+                //TODO
+        );
+    }
+}
+
