f0e42f390528658419d013c907dcb991efdfb504
nik <nikita.moiseienko@onix-systems.com>
2020-12-07 18:44:48 +0200

    PairedDevices

--- src/main/java/main_package/ui/__USER__/page_objects/home_header/home/account/PairedDevices.java
+++ src/main/java/main_package/ui/__USER__/page_objects/home_header/home/account/PairedDevices.java
@@ -7,10 +7,13 @@ import main_package.engine.test_engine.OnixUiAssert;
 import main_package.engine.ui_engine.OnixLocator;
 import main_package.engine.ui_engine.OnixPageObject;
 import main_package.engine.ui_engine.OnixWebDriver;
+import main_package.ui.__USER__.general_parts.Footer;
+import main_package.ui.__USER__.general_parts.home.account.AccountHeader;
+import main_package.ui.__USER__.general_parts.home.account.AccountSidebar;
 import org.openqa.selenium.By;
 import main_package.data.S;
 
-public class PairedDevices extends OnixPageObject {
+public class PairedDevices extends OnixPageObject implements AccountHeader, AccountSidebar, Footer {
     private String ENDPOINT_URL = ""; //TODO
     public PairedDevices(OnixWebDriver driver) {
         super(driver);
@@ -38,14 +41,18 @@ public class PairedDevices extends OnixPageObject {
     @Override
     public PairedDevices check(OnixUiAssert onixUiAssert) {
         for(OnixLocator l : OnixUiAssert.mergeArrays(
-                PairedDevices.Locator.values()
-                //TODO
+                PairedDevices.Locator.values(),
+                Footer.FooterLtr.values(),
+                AccountSidebar.AccountSidebarLtr.values(),
+                AccountHeader.AccountHeaderLtr.values()
         )) {
             onixUiAssert.softCheckCountOfElementByLocator(l, 1);
         }
         for(OnixLocator l : OnixUiAssert.mergeArrays(
-                PairedDevices.Locators.values()
-                //TODO
+                PairedDevices.Locators.values(),
+                Footer.FooterLtrs.values(),
+                AccountSidebar.AccountSidebarLtrs.values(),
+                AccountHeader.AccountHeaderLtrs.values()
         )) {
             onixUiAssert.softCheckMinimumOfElementsByLocator(l, 1);
         }
@@ -54,7 +61,7 @@ public class PairedDevices extends OnixPageObject {
 
 
     public enum Locator implements OnixLocator {
-        //TODO
+        CONNECT_TO_FITBIT_BUTTON(By.xpath("//a[contains(text(), 'Connect to Fitbit')]")),
         ;
         private By path;
         private S[] actions;
