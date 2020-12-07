74419a1281c83516f7fb0d3561343e788c53a006
nik <nikita.moiseienko@onix-systems.com>
2020-12-07 18:39:05 +0200

    ResetPsswordFromMyProfle

--- src/main/java/main_package/ui/__USER__/page_objects/home_header/home/account/my_profile/ResetPasswordFromMyProfile.java
+++ src/main/java/main_package/ui/__USER__/page_objects/home_header/home/account/my_profile/ResetPasswordFromMyProfile.java
@@ -7,10 +7,12 @@ import main_package.engine.test_engine.OnixUiAssert;
 import main_package.engine.ui_engine.OnixLocator;
 import main_package.engine.ui_engine.OnixPageObject;
 import main_package.engine.ui_engine.OnixWebDriver;
+import main_package.ui.__USER__.general_parts.Footer;
+import main_package.ui.__USER__.general_parts.home.account.AccountHeader;
 import org.openqa.selenium.By;
 import main_package.data.S;
 
-public class ResetPasswordFromMyProfile extends OnixPageObject {
+public class ResetPasswordFromMyProfile extends OnixPageObject implements Footer, AccountHeader {
     private String ENDPOINT_URL = ""; //TODO
     public ResetPasswordFromMyProfile(OnixWebDriver driver) {
         super(driver);
@@ -38,14 +40,16 @@ public class ResetPasswordFromMyProfile extends OnixPageObject {
     @Override
     public ResetPasswordFromMyProfile check(OnixUiAssert onixUiAssert) {
         for(OnixLocator l : OnixUiAssert.mergeArrays(
-                ResetPasswordFromMyProfile.Locator.values()
-                //TODO
+                ResetPasswordFromMyProfile.Locator.values(),
+                AccountHeader.AccountHeaderLtr.values(),
+                Footer.FooterLtr.values()
         )) {
             onixUiAssert.softCheckCountOfElementByLocator(l, 1);
         }
         for(OnixLocator l : OnixUiAssert.mergeArrays(
-                ResetPasswordFromMyProfile.Locators.values()
-                //TODO
+                ResetPasswordFromMyProfile.Locators.values(),
+                AccountHeader.AccountHeaderLtrs.values(),
+                Footer.FooterLtrs.values()
         )) {
             onixUiAssert.softCheckMinimumOfElementsByLocator(l, 1);
         }
@@ -54,7 +58,10 @@ public class ResetPasswordFromMyProfile extends OnixPageObject {
 
 
     public enum Locator implements OnixLocator {
-        //TODO
+        OLD_PASSWORD_INPUT(By.cssSelector("[name='old_password']")),
+        NEW_PASSWORD_INPUT(By.cssSelector("[name='new_password1']")),
+        NEW_PASSWORD_CONFIRM(By.cssSelector("[name='new_password2']")),
+        ENTER_BUTTON(By.cssSelector("form button")),
         ;
         private By path;
         private S[] actions;
