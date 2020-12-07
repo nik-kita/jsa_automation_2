9f28a7f9ae52119ce816a54e9a7362c0eab9c27f
nik <nikita.moiseienko@onix-systems.com>
2020-12-07 13:34:19 +0200

    Transformations

---
+++ src/test/java/test_package/ui/unit/ui/__GUEST__/page_objects/main/TransformationsTest.java
@@ -0,0 +1,50 @@
+
+package test_package.ui.unit.ui.__GUEST__.page_objects.main;
+
+
+import main_package.engine.test_engine.OnixUiTestRunner;
+import main_package.engine.ui_engine.OnixLocator;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import main_package.ui.__GUEST__.page_objects.main.Transformations;
+
+
+public class TransformationsTest extends OnixUiTestRunner {
+    Transformations transformations;
+    @BeforeClass
+    public void openTransformations() {
+        //TODO
+        //transformations = openSite();
+    }
+
+    @Test(dataProvider = "getUnique")
+    public void unique(OnixLocator locator) {
+        onixUiAssert.
+                checkCountOfElementByLocator(locator, 1);
+    }
+
+    @DataProvider
+    public Object[] getUnique() {
+        return mergeArrays(
+                Transformations.Locator.values()
+                //TODO
+        );
+    }
+
+    @Test(dataProvider = "getRepeated")
+    public void repeated(OnixLocator locator) {
+        onixUiAssert.
+                checkMinimumOfElementsByLocator(locator, 1);
+    }
+
+    @DataProvider
+    public Object[] getRepeated() {
+        return mergeArrays(
+                Transformations.Locators.values()
+                //TODO
+        );
+    }
+}
+
