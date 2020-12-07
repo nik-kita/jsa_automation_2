d9d7033e5801faab890182fa022805ab147b15b8
nik <nikita.moiseienko@onix-systems.com>
2020-12-07 13:59:36 +0200

    PrivacyPolicy

---
+++ src/test/java/test_package/ui/unit/ui/__GUEST__/page_objects/from_footer/PrivacyPolicyTest.java
@@ -0,0 +1,50 @@
+
+package test_package.ui.unit.ui.__GUEST__.page_objects.from_footer;
+
+
+import main_package.engine.test_engine.OnixUiTestRunner;
+import main_package.engine.ui_engine.OnixLocator;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import main_package.ui.__GUEST__.page_objects.from_footer.PrivacyPolicy;
+
+
+public class PrivacyPolicyTest extends OnixUiTestRunner {
+    PrivacyPolicy privacyPolicy;
+    @BeforeClass
+    public void openPrivacyPolicy() {
+        //TODO
+        //privacyPolicy = openSite();
+    }
+
+    @Test(dataProvider = "getUnique")
+    public void unique(OnixLocator locator) {
+        onixUiAssert.
+                checkCountOfElementByLocator(locator, 1);
+    }
+
+    @DataProvider
+    public Object[] getUnique() {
+        return mergeArrays(
+                PrivacyPolicy.Locator.values()
+                //TODO
+        );
+    }
+
+    @Test(dataProvider = "getRepeated")
+    public void repeated(OnixLocator locator) {
+        onixUiAssert.
+                checkMinimumOfElementsByLocator(locator, 1);
+    }
+
+    @DataProvider
+    public Object[] getRepeated() {
+        return mergeArrays(
+                PrivacyPolicy.Locators.values()
+                //TODO
+        );
+    }
+}
+
