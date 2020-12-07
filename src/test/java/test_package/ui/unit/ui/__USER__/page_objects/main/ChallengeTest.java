f61aaaf1d904854fdb5c454001672d16200a9f67
nik <nikita.moiseienko@onix-systems.com>
2020-12-07 15:42:08 +0200

    Challenge user

---
+++ src/test/java/test_package/ui/unit/ui/__USER__/page_objects/main/ChallengeTest.java
@@ -0,0 +1,50 @@
+
+package test_package.ui.unit.ui.__USER__.page_objects.main;
+
+
+import main_package.engine.test_engine.OnixUiTestRunner;
+import main_package.engine.ui_engine.OnixLocator;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import main_package.ui.__USER__.page_objects.main.Challenge;
+
+
+public class ChallengeTest extends OnixUiTestRunner {
+    Challenge challenge;
+    @BeforeClass
+    public void openChallenge() {
+        //TODO
+        //challenge = openSite();
+    }
+
+    @Test(dataProvider = "getUnique")
+    public void unique(OnixLocator locator) {
+        onixUiAssert.
+                checkCountOfElementByLocator(locator, 1);
+    }
+
+    @DataProvider
+    public Object[] getUnique() {
+        return mergeArrays(
+                Challenge.Locator.values()
+                //TODO
+        );
+    }
+
+    @Test(dataProvider = "getRepeated")
+    public void repeated(OnixLocator locator) {
+        onixUiAssert.
+                checkMinimumOfElementsByLocator(locator, 1);
+    }
+
+    @DataProvider
+    public Object[] getRepeated() {
+        return mergeArrays(
+                Challenge.Locators.values()
+                //TODO
+        );
+    }
+}
+
