07637282d6eb09f622e3eeaa013ec2e77da87686
nik <nikita.moiseienko@onix-systems.com>
2020-12-07 13:45:17 +0200

    MyPodcast

---
+++ src/test/java/test_package/ui/smoke/ui/__GUEST__/page_objects/main/my_podcast/MyPodcastTest.java
@@ -0,0 +1,19 @@
+
+package test_package.ui.smoke.ui.__GUEST__.page_objects.main.my_podcast;
+
+import main_package.engine.test_engine.OnixUiTestRunner;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import main_package.ui.__GUEST__.page_objects.main.my_podcast.MyPodcast;
+
+
+public class MyPodcastTest extends OnixUiTestRunner {
+    MyPodcast myPodcast;
+    @BeforeMethod
+    public void openMyPodcast() {
+        //TODO
+    }
+    //TODO
+}
+
