b6712908d7e14966fef9aaeff38b3a7feeb66c66
nik <nikita.moiseienko@onix-systems.com>
2020-12-07 15:00:41 +0200

    ResetPassword

---
+++ src/main/java/main_package/ui/__GUEST__/page_objects/main/login/ResetPassword.java
@@ -0,0 +1,101 @@
+
+package main_package.ui.__GUEST__.page_objects.main.login;
+
+import main_package.data.Settings;
+import main_package.engine.Fly;
+import main_package.engine.test_engine.OnixUiAssert;
+import main_package.engine.ui_engine.OnixLocator;
+import main_package.engine.ui_engine.OnixPageObject;
+import main_package.engine.ui_engine.OnixWebDriver;
+import org.openqa.selenium.By;
+import main_package.data.S;
+
+public class ResetPassword extends OnixPageObject {
+    private String ENDPOINT_URL = ""; //TODO
+    public ResetPassword(OnixWebDriver driver) {
+        super(driver);
+        log.debug("[{}] page is open", "ResetPassword"); //TODO
+    }
+
+
+    @Override
+    public ResetPassword make(Fly fly) {
+        fly.make();
+        return this;
+    }
+
+    @Override
+    public ResetPassword openFromScratch() {
+        driver.get(Settings.BASE_URL);
+        //TODO
+        return this;
+    }
+    @Override
+    public ResetPassword openFromUrl() {
+        driver.get(Settings.BASE_URL + ENDPOINT_URL);
+        return this;
+    }
+    @Override
+    public ResetPassword check(OnixUiAssert onixUiAssert) {
+        for(OnixLocator l : OnixUiAssert.mergeArrays(
+                ResetPassword.Locator.values()
+                //TODO
+        )) {
+            onixUiAssert.softCheckCountOfElementByLocator(l, 1);
+        }
+        for(OnixLocator l : OnixUiAssert.mergeArrays(
+                ResetPassword.Locators.values()
+                //TODO
+        )) {
+            onixUiAssert.softCheckMinimumOfElementsByLocator(l, 1);
+        }
+        return this;
+    }
+
+
+    public enum Locator implements OnixLocator {
+        //TODO
+        ;
+        private By path;
+        private S[] actions;
+        Locator(By path) {
+            this.path = path;
+        }
+        Locator(By path, S... actions) {
+            this.path = path;
+            this.actions = actions;
+        }
+        @Override
+        public By getPath() {
+            return path;
+        }
+        @Override
+        public S[] specialActions() {
+           return actions;
+        }
+    }
+
+    public enum Locators implements OnixLocator {
+        //TODO
+        ;
+        private By path;
+        private S[] actions;
+        Locators(By path) {
+            this.path = path;
+        }
+        Locators(By path, S... actions) {
+            this.path = path;
+            this.actions = actions;
+        }
+        @Override
+        public By getPath() {
+            return path;
+        }
+        @Override
+        public S[] specialActions() {
+           return actions;
+        }
+    }
+
+}
+
---
+++ src/test/java/test_package/ui/smoke/ui/__GUEST__/page_objects/main/login/ResetPasswordTest.java
@@ -0,0 +1,19 @@
+
+package test_package.ui.smoke.ui.__GUEST__.page_objects.main.login;
+
+import main_package.engine.test_engine.OnixUiTestRunner;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import main_package.ui.__GUEST__.page_objects.main.login.ResetPassword;
+
+
+public class ResetPasswordTest extends OnixUiTestRunner {
+    ResetPassword resetPassword;
+    @BeforeMethod
+    public void openResetPassword() {
+        //TODO
+    }
+    //TODO
+}
+
