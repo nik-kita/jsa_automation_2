aa043b6066a13056a75e1b134967e2fa2819bb63
nik <nikita.moiseienko@onix-systems.com>
2020-12-11 12:20:21 +0200

    CreateAccountTest

--- src/test/java/test_package/ui/smoke/ui/__GUEST__/page_objects/main/login/CreateAccountTest.java
+++ src/test/java/test_package/ui/smoke/ui/__GUEST__/page_objects/main/login/CreateAccountTest.java
@@ -12,8 +12,37 @@ public class CreateAccountTest extends OnixUiTestRunner {
     CreateAccount createAccount;
     @BeforeMethod
     public void openCreateAccount() {
-        //TODO
+        createAccount = openSite().clickHeaderJoinNow();
     }
-    //TODO
+    @Test
+    public void createAccountTest() {
+        createAccount.check(onixUiAssert);
+        onixUiAssert.assertAll();
+    }
+    @Test
+    public void clickJsaLogoTest() {
+        createAccount.clickJsaLogo().check(onixUiAssert);
+        onixUiAssert.assertAll();
+    }
+    @Test
+    public void clickSignInWithFacebook() {
+        createAccount.clickSignInWithFacebook().check(onixUiAssert);
+        onixUiAssert.assertAll();
+    }
+    @Test
+    public void clickSignInWithApple() {
+        createAccount.clickSignInWithApple().check(onixUiAssert);
+        onixUiAssert.assertAll();
+    }
+    @Test
+    public void clickSignIn() {
+        createAccount.clickSignInButton().check(onixUiAssert);
+        onixUiAssert.assertAll();
+    }
+    @Test
+    public void clickCreateAccountButton() {
+        //TODO
+        log.error("This test is not written!");
+    }
 }
 
